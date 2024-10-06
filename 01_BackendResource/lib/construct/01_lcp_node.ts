068f302efc34e9c565aeda147e1e3d26bf508e44
admin02-at-594175341170 <70833127+atakedemo@users.noreply.github.com>
2024-10-06 10:34:15 +0900

    AWS ECS設定の修正（ネットワークモードをawsvpc->bridgeへ変更）

--- 01_BackendResource/lib/construct/01_lcp_node.ts
+++ 01_BackendResource/lib/construct/01_lcp_node.ts
@@ -2,7 +2,8 @@ import * as ec2 from "aws-cdk-lib/aws-ec2";
 import * as cdk from 'aws-cdk-lib';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import { Repository } from "aws-cdk-lib/aws-ecr";
-import { AwsLogDriver, Cluster, ContainerImage, CpuArchitecture, FargateTaskDefinition, FargateService, OperatingSystemFamily } from "aws-cdk-lib/aws-ecs";
+import * as ecs from "aws-cdk-lib/aws-ecs";
+import * as autoscaling from 'aws-cdk-lib/aws-autoscaling';
 import { Construct } from 'constructs';
 
 export class LcpNode extends Construct {
@@ -25,19 +26,37 @@ export class LcpNode extends Construct {
     // --------------------
     // Security Group
     // --------------------
-    const securityGroup = new ec2.SecurityGroup(this, 'SecurityGroup', {
+    const securityGroupEc2 = new ec2.SecurityGroup(this, 'SecurityGroup', {
       vpc,
       description: 'IBC LPC Node',
       allowAllOutbound: true
     });
 
-    securityGroup.addIngressRule(
+    securityGroupEc2.addIngressRule(
       ec2.Peer.anyIpv4(),
       ec2.Port.tcp(443),
       'allow HTTPS traffic from anywhere',
     );
 
-    securityGroup.addIngressRule(
+    securityGroupEc2.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(80),
+      'allow HTTP traffic from anywhere',
+    );
+
+    const securityGroupCluster = new ec2.SecurityGroup(this, 'SecurityGroupCluster', {
+      vpc,
+      description: 'IBC LPC Node',
+      allowAllOutbound: true
+    });
+
+    securityGroupCluster.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(443),
+      'allow HTTPS traffic from anywhere',
+    );
+
+    securityGroupCluster.addIngressRule(
       ec2.Peer.anyIpv4(),
       ec2.Port.tcp(80),
       'allow HTTP traffic from anywhere',
@@ -69,18 +88,7 @@ export class LcpNode extends Construct {
       ]
     });
 
-    // const servicePolicy = new iam.Policy(
-    //   this,
-    //   'AppSyncInlinePolicy', {
-    //     policyName: 'AppSyncInlinePolicy',
-    //     statements: [
-    //       statementDescriptionLogGr,
-    //       statementSsm
-    //     ]
-    //   }
-    // )
-    
-    const role = new iam.Role(this, 'lpcEcsRole', {
+    const ecsTaskRole = new iam.Role(this, 'lpcEcsRole', {
       assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com'),
       managedPolicies: [
         iam.ManagedPolicy.fromAwsManagedPolicyName(
@@ -88,56 +96,73 @@ export class LcpNode extends Construct {
         ),
         iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonSSMReadOnlyAccess"),
         iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonEC2ContainerRegistryReadOnly"),
+        iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonSSMManagedInstanceCore"),
       ],
     })
-    role.addToPolicy(statementSsm);
-    role.addToPolicy(statementDescriptionLogGr);
+    ecsTaskRole.addToPolicy(statementSsm);
+    ecsTaskRole.addToPolicy(statementDescriptionLogGr);
+
+    // EC2インスタンス用のIAMロールを作成してSSMポリシーをアタッチ
+    const ec2Role = new iam.Role(this, 'Ec2InstanceRole', {
+      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
+    });
+
+    // SSMのアクセスを許可するポリシーを追加
+    ec2Role.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore'));
+
+    // --------------------
+    // EC2
+    // --------------------
+    const instanceType = new ec2.InstanceType('t3.large');
+    const autoScalingGroup = new ecs.AsgCapacityProvider(this, 'IbcAsgCapacityProvider', {
+      autoScalingGroup: new autoscaling.AutoScalingGroup(this, 'IbcEcsAsg', {
+        vpc,
+        vpcSubnets: vpc.selectSubnets({ subnetType: ec2.SubnetType.PUBLIC }),
+        instanceType,
+        machineImage: ecs.EcsOptimizedImage.amazonLinux2(
+          ecs.AmiHardwareType.STANDARD,
+        ),
+        minCapacity: 1,
+        maxCapacity: 1,
+        role: ec2Role,
+        securityGroup: securityGroupEc2
+      }),
+    });
+
 
     // --------------------
     // AWS ECS & ECR
     // --------------------
     // ECS Cluster
-    const cluster = new Cluster(this, "EcsCluster", {
+    const cluster = new ecs.Cluster(this, "EcsCluster", {
       clusterName: `ibc-demo-ethereum-cosmos`,
       vpc: vpc,
     });
+    cluster.addAsgCapacityProvider(autoScalingGroup);
 
-    // ECS Task-definition
-    const taskDefinition = new FargateTaskDefinition(this, "EcsTaskDefinition", {
-      cpu: 512,
-      memoryLimitMiB: 1024,
-      runtimePlatform: {
-        operatingSystemFamily: OperatingSystemFamily.LINUX,
-        cpuArchitecture: CpuArchitecture.X86_64,
-      },
+    const taskDefinition = new ecs.Ec2TaskDefinition(this, 'TaskDef', {
+      networkMode: ecs.NetworkMode.BRIDGE,
+      taskRole: ecsTaskRole,
     });
     
     taskDefinition.addContainer("LcpNodeContainer", {
-      image: ContainerImage.fromEcrRepository(_repositry),
+      image: ecs.ContainerImage.fromEcrRepository(_repositry),
+      cpu: 512,
+      memoryLimitMiB: 1024,
       portMappings: [
         { containerPort: 80 },
         { containerPort: 443 }
       ],
-      logging: new AwsLogDriver({
+      logging: new ecs.AwsLogDriver({
         streamPrefix: "LcpNode",
       }),
     });
 
-    const fargateService = new FargateService(this, "EcsFargateService", {
+    const ec2Service = new ecs.Ec2Service(this, 'Ec2Service', {
       cluster,
       taskDefinition,
-      desiredCount: 1,
-      securityGroups: [securityGroup],
-      vpcSubnets: vpc.selectSubnets({ subnetType: ec2.SubnetType.PUBLIC }),
-      assignPublicIp: true,
-    });
-
-    new cdk.CfnOutput(this, "ClusterName", {
-      value: fargateService.cluster.clusterName,
-    });
-
-    new cdk.CfnOutput(this, "ClusterArn", {
-      value: fargateService.cluster.clusterArn,
+      // securityGroups: [securityGroupCluster],
+      // vpcSubnets: vpc.selectSubnets({ subnetType: ec2.SubnetType.PUBLIC }),
     });
   }
 }
