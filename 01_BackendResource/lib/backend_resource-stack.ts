569273cd8b19e443bcca3d431e853612ea9c4c91
admin02-at-594175341170 <70833127+atakedemo@users.noreply.github.com>
2024-10-23 12:55:33 +0900

    AWS構成の変更（ECS -> EC2）

--- 01_BackendResource/lib/backend_resource-stack.ts
+++ 01_BackendResource/lib/backend_resource-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { DevInstance } from './construct/00_dev_instance';
 import { EcrStack } from './construct/02_ecr';
 import { LcpNode } from './construct/01_lcp_node';
 
@@ -8,8 +9,8 @@ export class BackendResourceStack extends cdk.Stack {
     super(scope, id, props);
 
     // const vpc = new Vpc(this, "Vpc", resourceName);
-    const ecr = new EcrStack(this, 'LcpNodeRcr');
-    console.log(ecr._repositry.repositoryUri);
-    const lcpNode = new LcpNode(this, 'LcpNodeRes', ecr._repositry);
+    // const ecr = new EcrStack(this, 'LcpNodeRcr');
+    // const lcpNode = new LcpNode(this, 'LcpNodeRes', ecr._repositry);
+    new DevInstance(this, 'DevInstance', 'ap-northeast-1', 'ami-0eba6c58b7918d3a1')
   }
 }
---
+++ 01_BackendResource/lib/construct/00_dev_instance.ts
@@ -0,0 +1,133 @@
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as cdk from 'aws-cdk-lib';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as ecs from "aws-cdk-lib/aws-ecs";
+import * as autoscaling from 'aws-cdk-lib/aws-autoscaling';
+import { Construct } from 'constructs';
+
+export class DevInstance extends Construct {
+  constructor(scope: Construct, id: string, region: string, amiId: string) {
+    super(scope, id);
+
+    // --------------------
+    // VPC
+    // --------------------
+    // Create new VPC with 2 Subnets
+    const vpc = new ec2.Vpc(this, 'VPC', {
+      natGateways: 0,
+      subnetConfiguration: [{
+        cidrMask: 24,
+        name: "asterisk",
+        subnetType: ec2.SubnetType.PUBLIC
+      }]
+    });
+
+    // --------------------
+    // Security Group
+    // --------------------
+    const securityGroupEc2 = new ec2.SecurityGroup(this, 'SecurityGroup', {
+      vpc,
+      description: 'IBC LPC Node',
+      allowAllOutbound: true
+    });
+
+    securityGroupEc2.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(443),
+      'allow HTTPS traffic from anywhere',
+    );
+
+    securityGroupEc2.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(80),
+      'allow HTTP traffic from anywhere',
+    );
+
+    const securityGroupCluster = new ec2.SecurityGroup(this, 'SecurityGroupCluster', {
+      vpc,
+      description: 'IBC LPC Node',
+      allowAllOutbound: true
+    });
+
+    securityGroupCluster.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(443),
+      'allow HTTPS traffic from anywhere',
+    );
+
+    securityGroupCluster.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(80),
+      'allow HTTP traffic from anywhere',
+    );
+
+    // --------------------
+    // IAM(Role, Policy)
+    // --------------------
+    const statementDescriptionLogGr = new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: [
+        "logs:DescribeLogGroups"
+      ],
+      resources: [
+        "*"
+      ]
+    });
+
+    const statementSsm = new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: [
+        "ssmmessages:CreateControlChannel",
+        "ssmmessages:CreateDataChannel",
+        "ssmmessages:OpenControlChannel",
+        "ssmmessages:OpenDataChannel"
+      ],
+      resources: [
+        "*"
+      ]
+    });
+
+    const ec2TackRole = new iam.Role(this, 'lpcEcsRole', {
+      assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com'),
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName(
+          "service-role/AmazonECSTaskExecutionRolePolicy"
+        ),
+        iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonSSMReadOnlyAccess"),
+        iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonEC2ContainerRegistryReadOnly"),
+        iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonSSMManagedInstanceCore"),
+      ],
+    })
+    ec2TackRole.addToPolicy(statementSsm);
+    ec2TackRole.addToPolicy(statementDescriptionLogGr);
+
+    // EC2インスタンス用のIAMロールを作成してSSMポリシーをアタッチ
+    const ec2Role = new iam.Role(this, 'Ec2InstanceRole', {
+      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
+    });
+
+    // SSMのアクセスを許可するポリシーを追加
+    ec2Role.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore'));
+
+    // --------------------
+    // EC2
+    // --------------------
+    const instanceType = new ec2.InstanceType('t3.xlarge');
+    const instance = new ec2.Instance(this, 'DevInstanceUbuntu', {
+        instanceType,
+        vpc,
+        vpcSubnets: vpc.selectSubnets({ subnetType: ec2.SubnetType.PUBLIC }),
+        machineImage: ec2.MachineImage.genericLinux({
+            ['ap-northeast-1']: 'ami-0eba6c58b7918d3a1',
+        }),
+        role: ec2Role,
+        securityGroup: securityGroupEc2,
+        blockDevices: [
+          {
+            deviceName: "/dev/sda1",
+            volume: ec2.BlockDeviceVolume.ebs(100),
+          },
+        ],
+    })
+  }
+}
